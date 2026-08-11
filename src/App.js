ac598e6c02a9f3063fc1809fd5ae4fa506fa6007
Dania Barakat <daniabarakat88@gmail.com>
2026-08-11 22:30:31 +0300

    Create initial portfolio content

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Button } from '@mui/material';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Welcome to My Portfolio</h1>
+      <p> Front-End Developer</p>
+      <Button variant='contained'> View My Work
+
+      </Button>
+    </div>
+  );
+}
+
+export default App;
